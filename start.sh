42f03801e1a6b0459d08223dbbfa9c24e43738c9
Gerda Iswari <64198931+gerdaiswari@users.noreply.github.com>
2022-12-07 11:21:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ip44="$(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')"
 cd /var/www/html
-php artisan serve --host=$ip44 --port=9455 &
-php artisan storage:link
+php artisan serve --host=172.17.0.8 --port=9455 & php artisan storage:link &
